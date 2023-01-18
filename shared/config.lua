c8458869c4f731d2ee3fa4564272f6f52b0e3871
SLW badsty <111683903+Badsty@users.noreply.github.com>
2023-01-18 14:35:04 +0100

    Add files via upload

---
+++ shared/config.lua
@@ -0,0 +1,15 @@
+Config = {}
+
+Config = {
+    AllowedLicences = {
+        ["license:ce041e6d7818da56b2f505947b15b05b47b6dde1"] = true -- votre license
+    },
+    TypeMarker = {Index = 1},
+    HeightMarker = {Index = 1},
+    RotateMarker = {IndexX = 1, IndexY = 1, IndexZ = 1},
+    ListColor = {IndexMarker = 1, IndexRect = 1, "Rouge", "Vert", "Bleu", "Opacité"},
+    CoordsList = {Index = 1, "vector3", "x, y, z", "{x, y, z}"},
+    DrawRectPostion = {X = 0.5, Y = 0.5},
+    DrawRectWidth = 0.5,
+    DrawRectHeight = 0.5,
+}
